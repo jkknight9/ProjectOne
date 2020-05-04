3c0bd3375ff1b2478687fa23eb075d0c2dc67b7e
Jack Knight <jkknight9@gmail.com>
2020-05-04 12:54:58 -0600

    Created viewController

---
+++ ProjectOne/PictureViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PictureViewController.swift
+//  ProjectOne
+//
+//  Created by Jack Knight on 5/4/20.
+//  Copyright © 2020 Jack Knight. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+   
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = false
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
