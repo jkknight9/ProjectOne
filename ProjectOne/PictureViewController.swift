4cd91942d8c1e30c0735513199cf0f0fff22565a
Jack Knight <jkknight9@gmail.com>
2020-07-08 08:46:44 -0600

    Added Activity controller

--- ProjectOne/PictureViewController.swift
+++ ProjectOne/PictureViewController.swift
@@ -29,12 +29,22 @@ class PictureViewController: UIViewController {
         
         title = "Picture: \(selectedPictureNumber) of \(totalPictures)"
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
-        // Do any additional setup after loading the view.
     }
     
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 1.0) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 
     /*
     // MARK: - Navigation
